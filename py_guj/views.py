82442ca9bee9c541d2f0fc856e379fbc94f64cdf
PP <prakhar126@gmail.com>
2014-07-22 00:13:11 +0530

    correct name

--- py_guj/views.py
+++ py_guj/views.py
@@ -8,4 +8,6 @@ from django.core.urlresolvers import reverse
 def home(request):
 	return render_to_response('py_guj/frontpage.html',context_instance=RequestContext(request))
 
+def meetings(request):
+	return render_to_response('py_guj/meetings.html',context_instance=RequestContext(request))
 
