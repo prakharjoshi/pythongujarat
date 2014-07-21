6e014848c6ebe0aef3fd83a76f33a55294472e89
PP <prakhar126@gmail.com>
2014-07-22 02:47:58 +0530

    basic layout

--- py_guj/urls.py
+++ py_guj/urls.py
@@ -7,8 +7,13 @@ from django.conf.urls import *
 
 urlpatterns = patterns('',
 	url(r'^$',views.home, name = 'home'),
-	url(r'/meetings^$',views.meetings, name = 'home'),
-	url(r'/blog^$',views.blog, name = 'home'),
-	url(r'/members^$',views.members, name = 'home'),
-	url(r'/resources^$',views.resources, name = 'home'),
+	url(r'^meetings/$',views.meetings, name = 'meetings'),
+	url(r'^blog/$',views.blog, name = 'home'),
+	url(r'^members/$',views.members, name = 'home'),
+	url(r'^resources/$',views.resources, name = 'home'),
+	url(r'^python-resources/$',views.pythonresources, name = 'pythonresources'),
+	url(r'^posting-policy/$',views.postingpolicy, name = 'postingpolicy'),
+	url(r'^git-and-pull-requests/$',views.gitandpullrequest, name = 'gitandpullrequests'),
+	url(r'^welcome/$',views.welcome, name = 'welcome'),
+	url(r'^launching-website/$',views.launchingwebsite, name = 'launchingwebsite'),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
