77ba0aae1969d92982e852143b81469f7ca12946
PP <prakhar126@gmail.com>
2014-08-19 00:01:50 +0530

    almost done

--- main.py
+++ main.py
@@ -102,3 +102,9 @@ app = webapp2.WSGIApplication([
 
 ############# End initialization ##############
 
+def main():
+    from paste import httpserver
+    httpserver.serve(app, host='127.0.0.1', port='8080')
+
+if __name__ == '__main__':
+    main()
